9520adce7f1e3890f33e208c96f141da3e67d2db
USER <USER@DESKTOP-4E5OOEL>
2025-12-05 11:29:55 +0200

    api finish

---
+++ tEsts/Job Salary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tEsts
+{
+    public class Job_Salary
+    {
+        public string location {  get; set; }
+        public string job_title { get; set; }
+        public int min_salary { get; set; }
+        public int max_salary { get; set; }
+        public int median_salary { get; set; }
+        public int min_base_salary { get; set; }
+        public int max_base_salary { get; set; }
+        public int median_base_salary { get; set; }
+        public int min_additional_pay { get; set; }
+        public int max_additional_pay { get; set; }
+        public int median_additional_pay { get; set; }
+        public string salary_period { get; set; }
+        public string salary_currency { get; set; }
+        public int salary_count { get; set; }
+        public string salaries_updated_at { get; set; }
+        public string publisher_Name { get; set; }
+        public string publisher_link { get; set; }
+        public string confidence { get; set; }
+    }
+}
--- tEsts/Program.cs
+++ tEsts/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.IsolatedStorage;
+﻿using System.Net.Http.Headers;
+using System.IO.IsolatedStorage;
 using System.Net.Http.Headers;
 using System.Text.Json;
 namespace tEsts
@@ -7,63 +8,48 @@ namespace tEsts
     {
         static void Main(string[] args)
         {
-            ConvertCurrency();
-            Console.ReadLine(); 
+            CompanyJobSalary();
+            Console.ReadLine();
         }
 
-        static async void ViewCurrencyList()
+        static async Task CompanyJobSalary() //השירות פותר את הצורך בסינון מדויק של נתוני שכר. הוא מאפשר להזין חברה, תפקיד, מיקום ורמת ניסיון — וככה לקבל מידע מותאם אישית במקום לחפש נתונים כלליים ולא רלוונטיים.
         {
-          
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri("https://currency-converter18.p.rapidapi.com/api/v1/supportedCurrencies"),
-                Headers =
-    {
-        { "x-rapidapi-key", "6af41fd3c5mshad393aab0dc6c95p14267djsn82645298eb38" },
-        { "x-rapidapi-host", "currency-converter18.p.rapidapi.com" },
-    },
-            };
-            using (var response = await client.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var body = await response.Content.ReadAsStringAsync();
-                List<Currency> list = JsonSerializer.Deserialize<List<Currency>>(body);
-                foreach(Currency currency in list)
-                {
-                    Console.WriteLine($"{currency.symbol} - {currency.name}");
-                }
-                
+            Console.WriteLine("insert company");
+            string company = Console.ReadLine(); //The company name for which to get salary information (e.g. Amazon).
+            Console.WriteLine("insert job title");
+            string job_title = Console.ReadLine(); ; //Job title for which to get salary estimation.
+            Console.WriteLine("insert location");
+            string location = Console.ReadLine(); ; //Free-text location/area in which to get salary estimation.
+            Console.WriteLine("insert location type");
+            string location_type = Console.ReadLine(); ; //Allowed values: ANY, CITY, STATE, COUNTRY
+            if (location_type == ""){
+                location_type = "ANY";
             }
-        }
-
-        static async void ConvertCurrency()
-        {
-
-            string from = "EUR";
-            string to = "USD";
-            int amount = 1000;
+            Console.WriteLine("insert years_of_experience");
+            string years_of_experience = Console.ReadLine(); ; //Allowed values: ALL, LESS_THAN_ONE, ONE_TO_THREE, FOUR_TO_SIX, SEVEN_TO_NINE, TEN_TO_FOURTEEN, ABOVE_FIFTEEN
+            if (years_of_experience == "")
+            {
+                years_of_experience = "ALL";
+            }
             var client = new HttpClient();
             var request = new HttpRequestMessage
             {
                 Method = HttpMethod.Get,
-                RequestUri = new Uri($"https://currency-converter18.p.rapidapi.com/api/v1/convert?from={from}&to={to}&amount={amount}"),
+                RequestUri = new Uri($"https://job-salary-data.p.rapidapi.com/company-job-salary?company={company}&job_title={job_title}&location={location}&location_type={location_type}&years_of_experience={years_of_experience}"),
                 Headers =
                 {
                     { "x-rapidapi-key", "6af41fd3c5mshad393aab0dc6c95p14267djsn82645298eb38" },
-                    { "x-rapidapi-host", "currency-converter18.p.rapidapi.com" },
+                    { "x-rapidapi-host", "job-salary-data.p.rapidapi.com" },
                 },
             };
+            
             using (var response = await client.SendAsync(request))
             {
                 response.EnsureSuccessStatusCode();
                 var body = await response.Content.ReadAsStringAsync();
+           
                 Console.WriteLine(body);
             }
         }
-        
-     
     }
 }
-//https://rapidapi.com/letscrape-6bRBa3QguO5/api/job-salary-data/playground/apiendpoint_15b153c8-1cc8-488b-acd9-7083754ce94c
